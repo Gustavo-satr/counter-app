92c82dc27f535532fef5aa017d5454fddf293063
Jose Gustavo Barrera Valerio <gus-jar@outlook.com>
2022-04-25 14:13:52 -0500

    DefaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,18 +3,23 @@ import PropTypes from 'prop-types';
 
 //import React, { Fragment } from 'react';
 
-const PrimeraApp = ( { saludo } ) => {
-
-
-
-    return (
-        <>
-@@ -16,4 +17,8 @@ const PrimeraApp = ( {saludo = 'Hola mundo'} ) => {
-
-}
-
-PrimeraApp.propTypes = {
-    saludo: PropTypes.string.isRequired
-}
-
-export default PrimeraApp; 
+    const PrimeraApp = ( { saludo, subtitulo } ) => {
+    
+    
+        return (
+            <>
+                <h1> {saludo} </h1>
+                {/* JSON.stringify(saludo, null, 3)*/ } 
+                <p>{ subtitulo }</p>
+            </>
+        );
+    
+    @@ -21,4 +21,8 @@ PrimeraApp.propTypes = {
+        saludo: PropTypes.string.isRequired
+    }
+    
+    PrimeraApp.defaultProps = {
+        subtitulo: 'Soy un subtitulo'
+    }
+    
+    export default PrimeraApp; 
