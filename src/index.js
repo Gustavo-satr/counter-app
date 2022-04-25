d2169cbcddbfb36ec872f2ad0681d8cbfb1bf221
Jose Gustavo Barrera Valerio <gus-jar@outlook.com>
2022-04-25 14:53:18 -0500

    Evento click

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+//import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 import './index.css';
+    //handleAdd
+    const handleAdd = (e) => {
+        console.log(e);
+    }
 
+    return (
+        <>
+          <h1>CounterApp</h1>
+          <h2> { value } </h2>
 
-
-const divRoot = document.querySelector('#app');
-
-
-
-ReactDom.render( <PrimeraApp /> , divRoot );
+          <button onClick =  {handleAdd } >+1</button>
+        </>
+    );
+}
+CounterApp.propTypes = {
+    value: PropTypes.number = {
+    }
+}
+export default CounterApp;
