243d20a5787c55490529bb54165295c826b039c1
Jose Gustavo Barrera Valerio <gus-jar@outlook.com>
2022-04-25 12:46:46 -0500

    Nuestro primer componente

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-
+import React from 'react';
 import ReactDom from 'react-dom';
+import PrimeraApp from './PrimeraApp';
 
+import './index.css';
 
 const saludo = <h1>Hola Mundo</h1>;
 
@@ -9,3 +11,4 @@ const divRoot = document.querySelector('#app');
 
 
 ReactDom.render( saludo, divRoot );
+ReactDom.render( <PrimeraApp /> , divRoot );
